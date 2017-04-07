43d0e11af12d27120cb56210301f08857e994869
camark <camark@live.cn>
2017-04-07 16:32:40 +0800

    pp

--- CSharpTools/Db/Sqlserver/DbUtil.cs
+++ CSharpTools/Db/Sqlserver/DbUtil.cs
@@ -10,11 +10,7 @@ namespace CSharpTools.Db.Sqlserver
     class DbUtil
     {
         public static SqlConnection getCon(string m_host, string m_db, string m_user, string m_pass)
-        {            
-            //m_host = "10.10.10.62";
-            //m_db = "bpm";
-            //m_user = "sa";
-            //m_pass = "china2015";
+        {                      
             return new SqlConnection(@"Data Source=" + m_host + @";Initial Catalog=" + m_db + @";User ID=" + m_user + @";Password=" + m_pass);
         }
     }
